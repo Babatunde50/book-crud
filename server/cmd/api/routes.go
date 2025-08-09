dee802b6e9ae06141a12087ba225b485d5553ac9
Babatunde50 <babatundeololade50@gmail.com>
2025-08-09 22:05:51 +0300

    feat(api): register book crud routes

--- server/cmd/api/routes.go
+++ server/cmd/api/routes.go
@@ -14,5 +14,11 @@ func (app *application) routes() http.Handler {
 
 	mux.HandlerFunc("GET", "/status", app.status)
 
+	mux.HandlerFunc("GET", "/books", app.listBooksHandler)
+	mux.HandlerFunc("POST", "/books", app.createBookHandler)
+	mux.HandlerFunc("GET", "/books/:id", app.showBookHandler)
+	mux.HandlerFunc("PUT", "/books/:id", app.updateBookHandler)
+	mux.HandlerFunc("DELETE", "/books/:id", app.deleteBookHandler)
+
 	return app.logAccess(app.recoverPanic(mux))
 }
